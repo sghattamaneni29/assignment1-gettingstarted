5ef4503cc06004c21f99209ac74c39ea7de1e516
sghattamaneni29 <98795597+sghattamaneni29@users.noreply.github.com>
2022-02-01 23:48:41 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,7 +11,7 @@ def welcome_assignment_answers(question):
         "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
         "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code": "42b76fe51778764973077a5a94056724",
         "Is MD5 a secured hashing algorithm? - Yes/No": "No",
-        "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number": 7,
+        "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number": 5,
         "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number": 4
     }
     return switcher.get(question, "No Matching Question to answer")
