8d7c77467b6362e1f4c98b0dd1f4068876d5bfe9
sghattamaneni29 <98795597+sghattamaneni29@users.noreply.github.com>
2022-02-01 23:34:24 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,10 +9,10 @@ def welcome_assignment_answers(question):
         "Is it possible to decrypt a message without a key? - Yes/No": "No",
         "Is it possible to decode a message without a key? - Yes/No": "Yes",
         "Is a hashed message supposed to be un-hashed? - Yes/No": "No",
-        "What is the MD5 hashing value to the following message: 'NYU Computer Networking'": "42b76fe51778764973077a5a94056724",
-        "Is MD5 a secured hashing algorithm? - Yes/No": "Yes",
-        "What layer from the TCP/IP model the protocol DHCP belongs to?": 7,
-        "What layer of the TCP/IP model the protocol TCP belongs to?": 4
+        "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code": "42b76fe51778764973077a5a94056724",
+        "Is MD5 a secured hashing algorithm? - Yes/No": "No",
+        "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number": 7,
+        "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number": 4
     }
     return switcher.get(question, "No Matching Question to answer")
 
